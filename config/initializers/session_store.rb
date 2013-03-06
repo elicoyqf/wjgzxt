2f4befb0843bf2ea40906332e2969cfb85225f18
elico <elico_yqf@sina.com>
2013-03-06 11:32:57 +0800

    重新修正了使用radmin相关的css的位置和方式，软件版本为v1.0

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Wjgzxt::Application.config.session_store :cookie_store, key: '_wjgzxt_session'
+Wjgzxt::Application.config.session_store :cookie_store, key: '_wjgzxt_session', expire_after: 60.minutes
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information

7c9f0aab5bc9fca6726c1a7389a4ab17fbf67512
elico <elico_yqf@sina.com>
2013-03-09 22:21:10 +0800

    修正了评分时边界的bug，软件版本为v1.5.4

--- lib/share/utiltily_tools.rb
+++ lib/share/utiltily_tools.rb
@@ -80,11 +80,11 @@ module UtiltilyTools
 
   def cal_method_1(cons_data, odata, ll, ul)
     sc = 0
-    if odata.to_s.to_f > cons_data.to_s.to_f * ll
+    if odata.to_s.to_f >= cons_data.to_s.to_f * ll
       sc = 1
-    elsif cons_data.to_s.to_f * ll <= odata.to_s.to_f && odata.to_s.to_f <= cons_data.to_s.to_f * ul
+    elsif cons_data.to_s.to_f * ll < odata.to_s.to_f && odata.to_s.to_f < cons_data.to_s.to_f * ul
       sc = 0
-    elsif odata.to_s.to_f < cons_data.to_s.to_f * ll
+    elsif odata.to_s.to_f <= cons_data.to_s.to_f * ll
       sc = -1
     end
     sc
@@ -92,11 +92,11 @@ module UtiltilyTools
 
   def cal_method_2(cons_data, odata, ll, ul)
     sc = 0
-    if odata.to_s.to_f < cons_data.to_s.to_f * ll
+    if odata.to_s.to_f <= cons_data.to_s.to_f * ll
       sc = 1
-    elsif cons_data.to_s.to_f * ll <= odata.to_s.to_f && odata.to_s.to_f <= cons_data.to_s.to_f * ul
+    elsif cons_data.to_s.to_f * ll < odata.to_s.to_f && odata.to_s.to_f < cons_data.to_s.to_f * ul
       sc = 0
-    elsif cons_data.to_s.to_f * ul < odata.to_s.to_f
+    elsif cons_data.to_s.to_f * ul <= odata.to_s.to_f
       sc = -1
     end
     sc

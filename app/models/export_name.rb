9e86dff67ac4ea7592075fee149865530fd303ff
elico <elico_yqf@sina.com>
2013-05-15 10:22:12 +0800

    添加了域名去重批量导入导出功能，软件版本为v1.8.6

--- app/models/export_name.rb
+++ app/models/export_name.rb
@@ -1,6 +1,8 @@
 #encoding : utf-8
 class ExportName < ActiveRecord::Base
   attr_accessible :name, :status, :user_id, :alias
+  validates :name, :uniqueness => true
+  validates :alias, :uniqueness => true
   validates :name, :uniqueness => { :scope => :alias }
   #validates :name, :presence => true
   #validates :name, :presence => { :message => '出口名称不能为空' }

d50bb906e46803a78984cb673c249aae567660bd
elico <elico_yqf@sina.com>
2013-08-27 23:58:27 +0800

    修正了显示原始数据时部分重叠的数据问题，软件版本为v1.9.8

--- app/controllers/rdata_controller.rb
+++ app/controllers/rdata_controller.rb
@@ -61,7 +61,7 @@ class RdataController < ApplicationController
     oth_arr.each do |line|
       tmp_arr = []
       tmp_arr << line
-      bbdata = HttpTestData.where('test_time >= ? and test_time < ? and source_node_name = ?  and  dest_url = ? ', time_begin, time_end, BACKBONE, line[2])
+      bbdata = HttpTestData.where('test_time >= ? and test_time < ? and source_node_name = ?  and  dest_url = ? and dest_locale in (?)', time_begin, time_end, BACKBONE, line[2], [' 电信', ' 联通'])
       unless bbdata.blank?
         if bbdata.size == 1
           tmp_arr << [bbdata.first.test_time, BACKBONE, bbdata.first.dest_url, bbdata.first.time_to_index, bbdata.first.total_time, bbdata.first.throughput_time, bbdata.first.connection_sr, bbdata.first.index_page_loading_sr]
